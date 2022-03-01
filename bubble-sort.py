97bea2de3f85433b7cdee4a81454a63ba3ca4801
Thomas Mosen <thomasmosen@gmail.com>
2022-03-01 10:40:05 +0100

    setup

---
+++ bubble-sort.py
@@ -0,0 +1,10 @@
+
+L = [8, 7, 6, 5, 4, 9]
+print(L)
+for i in range(len(L) - 1):
+    for j in range(0, len(L) - i - 1):
+        if L[j] > L[j + 1]:
+            L[j], L[j+1], = L[j+1], L[j]
+
+
+print(L)
